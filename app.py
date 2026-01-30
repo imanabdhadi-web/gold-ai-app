23bd84609776514988b4af3ddaa791d7eee53155
imanabdhadi-web <imanabdhadi@gmail.com>
2026-01-30 23:43:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ if uploaded_file:
         model = genai.GenerativeModel('gemini-1.5-flash')
         prompt = "Analisis gambar chart XAU/USD ini. Berikan: 1. Entry (Buy/Sell), 2. SL, 3. TP, 4. Volume (Besar/Kecil), 5. Tahap Keyakinan. Gunakan format yang kemas."
         response = model.generate_content([prompt, image])
-        st.write(result = response.text)
+        st.write(response.text)
       
